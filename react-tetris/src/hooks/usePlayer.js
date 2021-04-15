b9d20842b7a2c4eeae45489c5b3d7df8de304165
Morgan Williamson <morgantaylorwilliamson@protonmail.com>
2021-04-15 15:40:04 -0500

    Created rotation logic. Updated collision detection.

--- react-tetris/src/hooks/usePlayer.js
+++ react-tetris/src/hooks/usePlayer.js
@@ -1,6 +1,6 @@
 import { useState, useCallback } from "react";
 import { TETROMINOS, randomTetromino } from "../tetrominos";
-import { STAGE_WIDTH } from "../gameHelpers";
+import { STAGE_WIDTH, checkCollision } from "../gameHelpers";
 
 export const usePlayer = () => {
     const [player, setPlayer] = useState({
@@ -17,14 +17,49 @@ export const usePlayer = () => {
         }));
     };
 
-    // Only runs once. 
+    // Piece rotation functionality. (Reminder: tetrominos are matrices.)
+    const rotate = (matrix, dir) => {
+        // 1) Convert rows into columns:
+        const rotatedTetro = matrix.map((_, index) =>
+         matrix.map(col => col[index]),
+        );
+        // 2) Reverse each rox to get a rotated matrix (tetromino):
+        if (dir > 0) return rotatedTetro.map(row => row.reverse());
+        return rotatedTetro.reverse();
+    };
+
+    // Check collision when rotating piece. 
+    const playerRotate = (stage, dir) => {
+        // We don't want to mutate our state, so we create a clone...
+        const clonedPlayer = JSON.parse(JSON.stringify(player));
+        // ... and implement the rotation on that instead. 
+        clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir);
+
+        // Utilize an offset to ensure one piece doesn't rotate INTO another.
+        const pos = clonedPlayer.pos.x;
+        let offset = 1;
+        while(checkCollision(clonedPlayer, stage, { x: 0, y: 0 })) {
+            clonedPlayer.pos.x += offset;
+            offset = -(offset + (offset > 0 ? 1 : -1));
+            if (offset > clonedPlayer.tetromino[0].length) {
+                // Set the position back to the original position (pos).
+                rotate(clonedPlayer.tetromino, -dir);
+                clonedPlayer.pos.x = pos;
+                return;
+            };
+        };
+
+        setPlayer(clonedPlayer);
+    };
+
+    // Only runs once, clean slate.
     const resetPlayer = useCallback(() => {
         setPlayer({
             pos: { x: STAGE_WIDTH / 2 - 2, y: 0 },
             tetromino: randomTetromino().shape,
             collided: false,
-        })
-    }, [])
+        });
+    }, []);
 
-    return [player, updatePlayerPos, resetPlayer];
+    return [player, updatePlayerPos, resetPlayer, playerRotate];
 };
